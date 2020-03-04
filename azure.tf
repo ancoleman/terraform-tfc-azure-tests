82601c8ef573d46ece16abaefc4387296257a30d
Anton Coleman <acoleman@paloaltonetworks.com>
2020-03-04 01:06:17 -0600

    update for workspace tfc requirement

--- azure.tf
+++ azure.tf
@@ -2,7 +2,7 @@ provider "azurerm" {}
 
 
 locals {
-  workspace_split_region = split("+", terraform.workspace)
+  workspace_split_region = split("_", terraform.workspace)
   region                 = element(local.workspace_split_region, 1)
   workspace_split_environment = split("-", element(local.workspace_split_region, 0))
   environment                 = element(local.workspace_split_environment, 2)
